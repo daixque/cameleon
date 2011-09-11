1dded95de8bab394ce2a4eeeeedf2a6bc29f9046
daixque <daixque@gmail.com>
2011-09-11 12:16:53 +0900

    create default file on generate interface

--- lib/cameleon/cli/generate.rb
+++ lib/cameleon/cli/generate.rb
@@ -7,6 +7,11 @@ def generate
   path = ARGV.shift
   dir_path = File.join "response", path
   FileUtils.mkdir_p dir_path
+  
+  file_path = File.join dir_path, "default"
+  File.open(file_path, "w") do |f|
+    f.puts "edit me: #{file_path}"
+  end
 end
 
 generate
